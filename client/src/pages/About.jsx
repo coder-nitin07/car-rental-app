5ea0cecabaca98226f442ae897822bdb8524f5a2
coder-nitin07 <nitinthakur87504@gmail.com>
2025-10-30 18:46:43 +0530

    Create Contact Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <div className="mt-3 p-4" style={{ minHeight: '80vh' }}>
+        <h2 className='text-center mb-3'>About Us</h2>
+
+        <h4 className='text-center mb-3'>Car Rental App</h4>
+        <p className='text-align-justify'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit quibusdam repellat mollitia perspiciatis aliquam libero, provident vero eos. Dignissimos debitis nemo beatae totam quo dolor distinctio dolores necessitatibus, quae voluptatum cupiditate, fugit, minus fugiat accusamus accusantium nesciunt iure perspiciatis nisi atque! Nostrum, tempora a facilis obcaecati molestias repellat adipisci inventore facere voluptatibus, autem molestiae quibusdam neque enim mollitia qui corporis quas! Asperiores, rerum cum, temporibus quod quia vel sint a aliquid, inventore expedita repellendus itaque necessitatibus distinctio saepe facere tempore optio tempora blanditiis quaerat quo qui cupiditate et assumenda? Recusandae sit obcaecati quo quasi nihil nobis ea incidunt. Perspiciatis doloribus tempore impedit tenetur delectus, dolores ipsam, aspernatur voluptatibus, nulla consequuntur qui maxime a asperiores animi autem omnis facere provident. Fugiat ducimus nemo id veritatis aliquam labore, culpa suscipit, libero, distinctio eveniet repellendus sit nulla? Explicabo facere error earum aliquam sed provident ad ipsum hic debitis consequatur ipsam dolore, quibusdam illum iure ab, modi tenetur cupiditate rerum voluptate! Fugit, labore. Eveniet sunt quidem quis enim animi et est aperiam nemo placeat, ex repudiandae amet vero. Ducimus, voluptatibus rerum? Neque, quasi veritatis! Rem quaerat dolores totam nulla! Accusamus distinctio soluta atque quaerat laboriosam necessitatibus ut laudantium, voluptatem autem a, modi numquam eveniet!</p>
+      </div>
+    </>
   )
 }
 
--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+      <div className="d-flex flex-column align-items-center justify-content-center" style={{ minHeight: '80vh' }}>
+
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_jGGFtAUFB93mh8_97MEgrX6qn6SEs8a6JQ&s' />
+
+        <h4 className='text-center mb-3'>For Any Assistance </h4>
+
+        <h4 className='mt-3 text-success'>
+          <i className='fa-solid fa-phone-volume'></i> 0123456789
+        </h4>
+
+        <h4 className='mt-3'>
+          <i className='fa-solid fa-envelope'></i> help@carrentalapp.com
+        </h4>
+      </div>
+    </>
   )
 }
 
